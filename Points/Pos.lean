298fb4280c7c2fd1b04ae184def51920f9f9660c
Evgeny Kurnevsky <kurnevsky@gmail.com>
2024-08-23 22:59:03 +0200

    Use props instead of types.

--- Points/Pos.lean
+++ Points/Pos.lean
@@ -12,36 +12,36 @@ def sucx (pos: Pos width height): Pos width.succ height :=
 def sucy (pos: Pos width height): Pos width height.succ :=
   ⟨pos.fst, pos.snd.succ⟩
 
-inductive AdjacentRight: Pos width height → Pos width height → Type where
+inductive AdjacentRight: Pos width height → Pos width height → Prop where
   | adj_right: {width height: Nat} → @AdjacentRight width.succ.succ height.succ ⟨0, 0⟩ ⟨1, 0⟩
   | adj_to_right: {width height: Nat} → {pos₁ pos₂: Pos width height} → @AdjacentRight width height pos₁ pos₂ → @AdjacentRight width.succ height (sucx pos₁) (sucx pos₂)
   | adj_to_bottom: {width height: Nat} → {pos₁ pos₂: Pos width height} → @AdjacentRight width height pos₁ pos₂ → @AdjacentRight width height.succ (sucy pos₁) (sucy pos₂)
 
-inductive AdjacentBottom: Pos width height → Pos width height → Type where
+inductive AdjacentBottom: Pos width height → Pos width height → Prop where
   | adj_bottom: {width height: Nat} → @AdjacentBottom width.succ height.succ.succ ⟨0, 0⟩ ⟨0, 1⟩
   | adj_to_right: {width height: Nat} → {pos₁ pos₂: Pos width height} → @AdjacentBottom width height pos₁ pos₂ → @AdjacentBottom width.succ height (sucx pos₁) (sucx pos₂)
   | adj_to_bottom: {width height: Nat} → {pos₁ pos₂: Pos width height} → @AdjacentBottom width height pos₁ pos₂ → @AdjacentBottom width height.succ (sucy pos₁) (sucy pos₂)
 
-inductive AdjacentBottomRight: Pos width height → Pos width height → Type where
+inductive AdjacentBottomRight: Pos width height → Pos width height → Prop where
   | adj_bottom_right: {width height: Nat} → @AdjacentBottomRight width.succ.succ height.succ.succ ⟨0, 0⟩ ⟨1, 1⟩
   | adj_to_right: {width height: Nat} → {pos₁ pos₂: Pos width height} → @AdjacentBottomRight width height pos₁ pos₂ → @AdjacentBottomRight width.succ height (sucx pos₁) (sucx pos₂)
   | adj_to_bottom: {width height: Nat} → {pos₁ pos₂: Pos width height} → @AdjacentBottomRight width height pos₁ pos₂ → @AdjacentBottomRight width height.succ (sucy pos₁) (sucy pos₂)
 
-inductive AdjacentTopRight: Pos width height → Pos width height → Type where
+inductive AdjacentTopRight: Pos width height → Pos width height → Prop where
   | adj_top_right: {width height: Nat} → @AdjacentTopRight width.succ.succ height.succ.succ ⟨0, 1⟩ ⟨1, 0⟩
   | adj_to_right: {width height: Nat} → {pos₁ pos₂: Pos width height} → @AdjacentTopRight width height pos₁ pos₂ → @AdjacentTopRight width.succ height (sucx pos₁) (sucx pos₂)
   | adj_to_bottom: {width height: Nat} → {pos₁ pos₂: Pos width height} → @AdjacentTopRight width height pos₁ pos₂ → @AdjacentTopRight width height.succ (sucy pos₁) (sucy pos₂)
 
-noncomputable def AdjacentLeft (pos₁: Pos width height) (pos₂: Pos width height): Type :=
+noncomputable def AdjacentLeft (pos₁: Pos width height) (pos₂: Pos width height): Prop :=
   AdjacentRight pos₂ pos₁
 
-noncomputable def AdjacentTop (pos₁: Pos width height) (pos₂: Pos width height): Type :=
+noncomputable def AdjacentTop (pos₁: Pos width height) (pos₂: Pos width height): Prop :=
   AdjacentBottom pos₂ pos₁
 
-noncomputable def AdjacentTopLeft (pos₁: Pos width height) (pos₂: Pos width height): Type :=
+noncomputable def AdjacentTopLeft (pos₁: Pos width height) (pos₂: Pos width height): Prop :=
   AdjacentBottomRight pos₂ pos₁
 
-noncomputable def AdjacentBottomLeft (pos₁: Pos width height) (pos₂: Pos width height): Type :=
+noncomputable def AdjacentBottomLeft (pos₁: Pos width height) (pos₂: Pos width height): Prop :=
   AdjacentTopRight pos₂ pos₁
 
 def n (pos: Pos width height): Option (Pos width height) :=
