3ec9c3b78d57382cb4c85ccbbaed03fc0a52d373
Evgeny Kurnevsky <kurnevsky@gmail.com>
2024-08-24 13:10:08 +0200

    Use different encoding of adjacent props.

--- Points/Pos.lean
+++ Points/Pos.lean
@@ -6,62 +6,52 @@ def Pos (width height : Nat) : Type :=
 
 namespace Pos
 
-def sucx (pos: Pos width height): Pos width.succ height :=
-  ⟨pos.fst.succ, pos.snd⟩
+abbrev AdjacentRight (pos₁: Pos width height) (pos₂: Pos width height): Prop :=
+  match pos₁, pos₂ with
+  | ⟨x₁, y₁⟩, ⟨x₂, y₂⟩ => x₁.succ = x₂.castSucc ∧ y₁ = y₂
 
-def sucy (pos: Pos width height): Pos width height.succ :=
-  ⟨pos.fst, pos.snd.succ⟩
+abbrev AdjacentBottom (pos₁: Pos width height) (pos₂: Pos width height): Prop :=
+  match pos₁, pos₂ with
+  | ⟨x₁, y₁⟩, ⟨x₂, y₂⟩ => x₁ = x₂ ∧ y₁.succ = y₂.castSucc
 
-inductive AdjacentRight: Pos width height → Pos width height → Prop where
-  | adj_right: {width height: Nat} → @AdjacentRight width.succ.succ height.succ ⟨0, 0⟩ ⟨1, 0⟩
-  | adj_to_right: {width height: Nat} → {pos₁ pos₂: Pos width height} → @AdjacentRight width height pos₁ pos₂ → @AdjacentRight width.succ height (sucx pos₁) (sucx pos₂)
-  | adj_to_bottom: {width height: Nat} → {pos₁ pos₂: Pos width height} → @AdjacentRight width height pos₁ pos₂ → @AdjacentRight width height.succ (sucy pos₁) (sucy pos₂)
+abbrev AdjacentBottomRight (pos₁: Pos width height) (pos₂: Pos width height): Prop :=
+  match pos₁, pos₂ with
+  | ⟨x₁, y₁⟩, ⟨x₂, y₂⟩ => x₁.succ = x₂.castSucc ∧ y₁.succ = y₂.castSucc
 
-inductive AdjacentBottom: Pos width height → Pos width height → Prop where
-  | adj_bottom: {width height: Nat} → @AdjacentBottom width.succ height.succ.succ ⟨0, 0⟩ ⟨0, 1⟩
-  | adj_to_right: {width height: Nat} → {pos₁ pos₂: Pos width height} → @AdjacentBottom width height pos₁ pos₂ → @AdjacentBottom width.succ height (sucx pos₁) (sucx pos₂)
-  | adj_to_bottom: {width height: Nat} → {pos₁ pos₂: Pos width height} → @AdjacentBottom width height pos₁ pos₂ → @AdjacentBottom width height.succ (sucy pos₁) (sucy pos₂)
+abbrev AdjacentTopRight (pos₁: Pos width height) (pos₂: Pos width height): Prop :=
+  match pos₁, pos₂ with
+  | ⟨x₁, y₁⟩, ⟨x₂, y₂⟩ => x₁.succ = x₂.castSucc ∧ y₁.castSucc = y₂.succ
 
-inductive AdjacentBottomRight: Pos width height → Pos width height → Prop where
-  | adj_bottom_right: {width height: Nat} → @AdjacentBottomRight width.succ.succ height.succ.succ ⟨0, 0⟩ ⟨1, 1⟩
-  | adj_to_right: {width height: Nat} → {pos₁ pos₂: Pos width height} → @AdjacentBottomRight width height pos₁ pos₂ → @AdjacentBottomRight width.succ height (sucx pos₁) (sucx pos₂)
-  | adj_to_bottom: {width height: Nat} → {pos₁ pos₂: Pos width height} → @AdjacentBottomRight width height pos₁ pos₂ → @AdjacentBottomRight width height.succ (sucy pos₁) (sucy pos₂)
-
-inductive AdjacentTopRight: Pos width height → Pos width height → Prop where
-  | adj_top_right: {width height: Nat} → @AdjacentTopRight width.succ.succ height.succ.succ ⟨0, 1⟩ ⟨1, 0⟩
-  | adj_to_right: {width height: Nat} → {pos₁ pos₂: Pos width height} → @AdjacentTopRight width height pos₁ pos₂ → @AdjacentTopRight width.succ height (sucx pos₁) (sucx pos₂)
-  | adj_to_bottom: {width height: Nat} → {pos₁ pos₂: Pos width height} → @AdjacentTopRight width height pos₁ pos₂ → @AdjacentTopRight width height.succ (sucy pos₁) (sucy pos₂)
-
-noncomputable def AdjacentLeft (pos₁: Pos width height) (pos₂: Pos width height): Prop :=
+abbrev AdjacentLeft (pos₁: Pos width height) (pos₂: Pos width height): Prop :=
   AdjacentRight pos₂ pos₁
 
-noncomputable def AdjacentTop (pos₁: Pos width height) (pos₂: Pos width height): Prop :=
+abbrev AdjacentTop (pos₁: Pos width height) (pos₂: Pos width height): Prop :=
   AdjacentBottom pos₂ pos₁
 
-noncomputable def AdjacentTopLeft (pos₁: Pos width height) (pos₂: Pos width height): Prop :=
+abbrev AdjacentTopLeft (pos₁: Pos width height) (pos₂: Pos width height): Prop :=
   AdjacentBottomRight pos₂ pos₁
 
-noncomputable def AdjacentBottomLeft (pos₁: Pos width height) (pos₂: Pos width height): Prop :=
+abbrev AdjacentBottomLeft (pos₁: Pos width height) (pos₂: Pos width height): Prop :=
   AdjacentTopRight pos₂ pos₁
 
-def n (pos: Pos width height): Option (Pos width height) :=
-  match pos with
+def n (pos₁: Pos width height): Option $ Σ' pos₂: Pos width height, AdjacentTop pos₁ pos₂ :=
+  match pos₁ with
   | ⟨_, ⟨0, _⟩⟩ => Option.none
-  | ⟨x, ⟨Nat.succ y, h⟩⟩ => Option.some ⟨x, ⟨y, Nat.lt_of_succ_lt h⟩⟩
+  | ⟨x, ⟨Nat.succ y, h⟩⟩ => Option.some ⟨⟨x, ⟨y, Nat.lt_of_succ_lt h⟩⟩, by apply And.intro; rfl; rfl⟩
 
-def s (pos: Pos width height): Option (Pos width height) :=
-  match height, pos with
-  | 1, ⟨_, ⟨0, _⟩⟩ => Option.none
-  | Nat.succ (Nat.succ _), ⟨x, ⟨0, _⟩⟩ => Option.some ⟨x, 1⟩
-  | Nat.succ _, ⟨x, ⟨Nat.succ y, h⟩⟩ => (Pos.s ⟨x, ⟨y, Nat.lt_of_succ_lt_succ h⟩⟩).map fun ⟨x₁, y₁⟩ => ⟨x₁, y₁.succ⟩
+def s (pos₁: Pos width height): Option $ Σ' pos₂: Pos width height, AdjacentBottom pos₁ pos₂ :=
+  let y := ↑pos₁.snd.succ
+  if h: y < height
+  then Option.some ⟨⟨pos₁.fst, ⟨y, h⟩⟩, by apply And.intro; rfl; rfl⟩
+  else Option.none
 
-def w (pos: Pos width height): Option (Pos width height) :=
-  match pos with
+def w (pos₁: Pos width height): Option $ Σ' pos₂: Pos width height, AdjacentLeft pos₁ pos₂ :=
+  match pos₁ with
   | ⟨⟨0, _⟩, _⟩ => Option.none
-  | ⟨⟨Nat.succ x, h⟩, y⟩ => Option.some ⟨⟨x, Nat.lt_of_succ_lt h⟩, y⟩
+  | ⟨⟨Nat.succ x, h⟩, y⟩ => Option.some ⟨⟨⟨x, Nat.lt_of_succ_lt h⟩, y⟩, by apply And.intro; rfl; rfl⟩
 
-def e (pos: Pos width height): Option (Pos width height) :=
-  match width, pos with
-  | 1, ⟨⟨0, _⟩, _⟩ => Option.none
-  | Nat.succ (Nat.succ _), ⟨⟨0, _⟩, y⟩ => Option.some ⟨1, y⟩
-  | Nat.succ _, ⟨⟨Nat.succ x, h⟩, y⟩ => (Pos.s ⟨⟨x, Nat.lt_of_succ_lt_succ h⟩, y⟩).map fun ⟨x₁, y₁⟩ => ⟨x₁.succ, y₁⟩
+def e (pos₁: Pos width height): Option $ Σ' pos₂: Pos width height, AdjacentRight pos₁ pos₂ :=
+  let x := ↑pos₁.fst.succ
+  if h: x < width
+  then Option.some ⟨⟨⟨x, h⟩, pos₁.snd⟩, by apply And.intro; rfl; rfl⟩
+  else Option.none
