00c4f868ce36b480e9469cd2bbb1a52db2da5cf8
Evgeny Kurnevsky <kurnevsky@gmail.com>
2024-08-25 20:46:27 +0200

    Add helper methods.

--- Points/Field.lean
+++ Points/Field.lean
@@ -12,6 +12,21 @@ structure Field where
   points: Array Point -- TODO: use Vector from Batteries when it's available in release
   size_eq: points.size = width * height
 
+def cell (field: @Field width height) (pos: Pos width height): Point :=
+  field.points.get <| pos.toFin.cast field.size_eq.symm
+
+def isPuttingAllowed (field: @Field width height) (pos: Pos width height): Bool :=
+  (cell field pos).isPuttingAllowed
+
+def isPlayer (field: @Field width height) (pos: Pos width height) (player: Player): Bool :=
+  (cell field pos).isPlayer player
+
+def isPlayersPoint (field: @Field width height) (pos: Pos width height) (player: Player): Bool :=
+  (cell field pos).isPlayersPoint player
+
+def isCapturedPoint (field: @Field width height) (pos: Pos width height) (player: Player): Bool :=
+  (cell field pos).isCapturedPoint player
+
 def emptyField: @Field width height :=
   { scoreRed := 0
   , scoreBlack := 0
