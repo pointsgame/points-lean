c395a47076b788a54b7ac6a8d4f8aae442ddb8d9
Evgeny Kurnevsky <kurnevsky@gmail.com>
2024-08-25 18:59:36 +0200

    Add size_eq.

--- Points/Field.lean
+++ Points/Field.lean
@@ -2,17 +2,21 @@ import Points.Pos
 import Points.Player
 import Points.Point
 
+variable {width height: Nat}
+
 structure Field where
   scoreRed: Nat
   scoreBlack: Nat
   moves: List (Pos width height × Player)
   lastSurroundPlayer: Player
-  points: Array Point -- TODO: Vector from Batteries
+  points: Array Point -- TODO: use Vector from Batteries when it's available in release
+  size_eq: points.size = width * height
 
-def emptyField {width height: Nat}: Field :=
+def emptyField: @Field width height :=
   { scoreRed := 0
   , scoreBlack := 0
   , moves := []
   , lastSurroundPlayer := Player.red
   , points := mkArray (width * height) Point.EmptyPoint
+  , size_eq := Array.size_mkArray ..
   }
