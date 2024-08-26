1d490aa306b1cd6708ea3ad2ecf4abdc81bfda79
Evgeny Kurnevsky <kurnevsky@gmail.com>
2024-08-26 09:38:47 +0200

    Implement wave.

--- Points/Field.lean
+++ Points/Field.lean
@@ -35,3 +35,30 @@ def emptyField: @Field width height :=
   , points := mkArray (width * height) Point.EmptyPoint
   , size_eq := Array.size_mkArray ..
   }
+
+def wave [Monad m] (startPos: Pos width height) (f: Pos width height → m Bool): m Unit := do
+  let neighborhood (pos: Pos width height): List $ Pos width height :=
+    [pos.n', pos.s', pos.w', pos.e'].filterMap (·.map (·.fst))
+  unless ← f startPos do
+    return
+  let mut q := Std.Queue.empty
+  q := q.enqueue startPos
+  repeat
+    if let Option.some ⟨next, rest⟩ := q.dequeue? then
+      q := rest
+      for pos in neighborhood next do
+        if ← f pos then
+          q := q.enqueue pos
+    else
+      break
+  return
+
+def wave' (startPos: Pos width height) (f: Pos width height → Bool): Lean.HashSet $ Pos width height :=
+  let fState (pos: Pos width height): StateM (Lean.HashSet $ Pos width height) Bool := do
+    let passed ← StateT.get
+    if !passed.contains pos && f pos then
+      StateT.set $ passed.insert pos
+      return true
+    else
+      return false
+  (StateT.run (wave startPos fState) Lean.HashSet.empty).snd
